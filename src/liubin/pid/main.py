1e0ff214ddc4172f6b5fad9e8b36e045921034ae
刘斌 <liu_bin4835@outlook.com>
2023-11-27 14:55:17 +0800

    pid file 注册信号处理函数

--- src/liubin/pid/main.py
+++ src/liubin/pid/main.py
@@ -3,31 +3,46 @@ about pid file
 """
 import atexit
 import os
+import signal
 import time
 
+pid_file_path: str = "your_project.pid"  # 替换为你期望的 PID 文件路径
 
-def write_pid_file(file_path):
+
+def write_pid_file():
     pid = os.getpid()
-    with open(file_path, "w", encoding="utf-8") as pid_file:
+    with open(pid_file_path, "w", encoding="utf-8") as pid_file:
         pid_file.write(str(pid))
 
 
-def remove_pid_file(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        print(f"PID file removed: {file_path}")
+def remove_pid_file(sig_id, frame):
+    sig = {x.value: x for x in signal.valid_signals()}.get(sig_id)
+    print(f"remove_pid_file {sig.name}, {sig_id=}, {frame=}")
+    if os.path.exists(pid_file_path):
+        os.remove(pid_file_path)
+        print(f"PID file removed: {pid_file_path}")
+    exit(0)
+
+
+def remove_pid_file_at_exit():
+    print("remove_pid_file_at_exit")
+    if os.path.exists(pid_file_path):
+        os.remove(pid_file_path)
+        print(f"PID file removed: {pid_file_path}")
 
 
 if __name__ == "__main__":
-    pid_file_path = "your_project.pid"  # 替换为你期望的 PID 文件路径
-
     try:
-        write_pid_file(pid_file_path)
+        write_pid_file()
         print(f"PID file created: {pid_file_path}")
 
         # 注册退出时执行的函数
-        atexit.register(remove_pid_file, pid_file_path)
+        atexit.register(remove_pid_file_at_exit)
 
+        # 注册信号处理函数
+        signal.signal(signal.SIGTERM, remove_pid_file)
+        signal.signal(signal.SIGINT, remove_pid_file)
+        signal.signal(signal.SIGKILL, remove_pid_file)
         # 你的程序逻辑可以放在这里
 
         time.sleep(60)
